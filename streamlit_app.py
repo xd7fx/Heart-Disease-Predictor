2b7f25a55a09a3f2436709728e97e728c2184690
Abdulrahman Alnashri <d7.fx99@gmail.com>
2024-11-01 01:21:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,18 +10,27 @@ st.title('Heart Disease Prediction System')
 
 # Collect user input for model features
 age = st.number_input('Age', min_value=1, max_value=120)
-sex = st.selectbox('Sex (0: Female, 1: Male)', [0, 1])
-cp = st.selectbox('Chest Pain Type (0-3)', [0, 1, 2, 3])
+sex = st.selectbox('Sex', ['Female', 'Male'])
+cp = st.selectbox('Chest Pain Type', ['Typical Angina', 'Atypical Angina', 'Non-Anginal Pain', 'Asymptomatic'])
 trestbps = st.number_input('Resting Blood Pressure', min_value=80, max_value=200)
 chol = st.number_input('Cholesterol Level', min_value=100, max_value=400)
-fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl (1: Yes, 0: No)', [0, 1])
-restecg = st.selectbox('Resting Electrocardiographic Results (0-2)', [0, 1, 2])
+fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', ['No', 'Yes'])
+restecg = st.selectbox('Resting Electrocardiographic Results', ['Normal', 'Having ST-T Wave Abnormality', 'Showing Probable or Definite Left Ventricular Hypertrophy'])
 thalach = st.number_input('Maximum Heart Rate Achieved', min_value=60, max_value=220)
-exang = st.selectbox('Exercise Induced Angina (1: Yes, 0: No)', [0, 1])
+exang = st.selectbox('Exercise Induced Angina', ['No', 'Yes'])
 oldpeak = st.number_input('ST Depression Induced by Exercise')
-slope = st.selectbox('Slope of the Peak Exercise ST Segment (0-2)', [0, 1, 2])
-ca = st.selectbox('Number of Major Vessels (0-4)', [0, 1, 2, 3, 4])
-thal = st.selectbox('Thalassemia (0-3)', [0, 1, 2, 3])
+slope = st.selectbox('Slope of the Peak Exercise ST Segment', ['Upsloping', 'Flat', 'Downsloping'])
+ca = st.selectbox('Number of Major Vessels', [0, 1, 2, 3, 4])
+thal = st.selectbox('Thalassemia', ['Normal', 'Fixed Defect', 'Reversible Defect'])
+
+# Map text inputs to numerical values for the model
+sex = 1 if sex == 'Male' else 0
+cp = ['Typical Angina', 'Atypical Angina', 'Non-Anginal Pain', 'Asymptomatic'].index(cp)
+fbs = 1 if fbs == 'Yes' else 0
+restecg = ['Normal', 'Having ST-T Wave Abnormality', 'Showing Probable or Definite Left Ventricular Hypertrophy'].index(restecg)
+exang = 1 if exang == 'Yes' else 0
+slope = ['Upsloping', 'Flat', 'Downsloping'].index(slope)
+thal = ['Normal', 'Fixed Defect', 'Reversible Defect'].index(thal)
 
 # Prepare input data for prediction
 input_data = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
